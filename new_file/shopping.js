f910179bc8cbe600e6dd3147339924fcbce179db
Suraj Hattarakal <suriisuraj8@gmail.com>
2025-10-17 21:21:08 +0530

    Add files via upload

---
+++ new_file/shopping.js
@@ -0,0 +1,25 @@
+// Assuming you saved logos in "images" folder: images/amazon.png, images/flipkart.png, etc.
+const shoppingSites = [
+    {name: "Amazon India", url: "https://www.amazon.in", logo: "images/amazon.png"},
+    {name: "Flipkart", url: "https://www.flipkart.com", logo: "images/flipkart.png"},
+    {name: "Myntra", url: "https://www.myntra.com", logo: "images/myntra.png"},
+    {name: "Snapdeal", url: "https://www.snapdeal.com", logo: "images/snapdeal.png"},
+    {name: "Tata Cliq", url: "https://www.tatacliq.com", logo: "images/tatacliq.png"},
+    {name: "ShopClues", url: "https://www.shopclues.com", logo: "images/shopclues.png"},
+    {name: "Paytm Mall", url: "https://www.paytmmall.com", logo: "images/paytmmall.png"},
+    {name: "BigBasket", url: "https://www.bigbasket.com", logo: "images/bigbasket.png"},
+    {name: "Grofers", url: "https://www.grofers.com", logo: "images/grofers.png"},
+    {name: "Limeroad", url: "https://www.limeroad.com", logo: "images/limeroad.png"},
+    {name: "FabIndia", url: "https://www.fabindia.com", logo: "images/fabindia.png"},
+    {name: "Croma", url: "https://www.croma.com", logo: "images/croma.png"},
+    {name: "Ajio", url: "https://www.ajio.com", logo: "images/ajio.png"},
+    {name: "Nykaa", url: "https://www.nykaa.com", logo: "images/nykaa.png"},
+    {name: "Pepperfry", url: "https://www.pepperfry.com", logo: "images/pepperfry.png"},
+    {name: "FirstCry", url: "https://www.firstcry.com", logo: "images/firstcry.png"},
+    {name: "Sangeet Electronics", url: "https://www.sangeet.in", logo: "images/sangeet.png"},
+    {name: "Porrvika", url: "https://www.porrvika.com", logo: "images/porrvika.png"},
+    {name: "Apollo Pharmacy", url: "https://www.apollopharmacy.in", logo: "images/apollo.png"},
+    {name: "Medlife", url: "https://www.medlife.com", logo: "images/medlife.png"}
+];
+
+
